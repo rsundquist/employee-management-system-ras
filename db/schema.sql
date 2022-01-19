96a6dcc5d0852ab015e4b0e39f66cae03d5cdaa3
Rebecca Sundquist <rsundquist629@gmail.com>
2022-01-18 20:04:41 -0500

    updating schema seeds and add from previous repo

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    department_name VARCHAR(30) UNIQUE NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roles_id INT,
+    manager_id INT,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_roles FOREIGN KEY(roles_id) REFERENCES roles(id) ON DELETE CASCADE,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+); 
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO department (department_name)
+VALUES ('Testing Department'),
+       ('Demo Department');
+
+INSERT INTO roles (title, salary)
+VALUES ('Manager',100000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES('Rebecca','Sundquist');
